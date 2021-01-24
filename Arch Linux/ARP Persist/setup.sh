3ba2228dcf5b63fef51ae950c1819d6e20dd1fca
Parvesh Monu <parvesh.dhullmonu@gmail.com>
2021-01-24 17:27:50 +0530

    deleted old files

--- Arch Linux/ARP Persist/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sudo cp arp_persist.py /usr/bin
-sudo chmod +x arp_persist.py
-sudo cp arp_persist.service /etc/systemd/system
-sudo systemctl enable arp_persist
-sudo systemctl start arp_persist
