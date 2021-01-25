4f8fcd1adb3a753555f96e7bf1e495eda55d407e
Parvesh Monu <parvesh.dhullmonu@gmail.com>
2021-01-25 22:54:24 +0530

    Update github-repo-sum.sh

--- github/github-repo-sum.sh
+++ github/github-repo-sum.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# Set variables
-CNTX={users|orgs}
-NAME={username|orgname}
-PAGE=1
-
-# Size in KB
-curl "https://api.github.com/$CNTX/$NAME/repos?page=$PAGE&per_page=100"| grep -e 'git_url*' | cut -d / -f 5 | cut -d . -f 1 | xargs -L1 -i curl https://api.github.com/repos/$NAME/{} 2> /dev/null | grep size | tr -dc '[:digit:]\n' | paste -sd+ - | bc
+if [[ $# -lt 3 ]]
+then
+	echo "Usage: program [users|orgs] [username|orgname] page"
+else
+	CNTX=$1
+	NAME=$2
+	PAGE=$3
+	curl "https://api.github.com/$CNTX/$NAME/repos?page=$PAGE&per_page=100"| grep -e 'git_url*' | cut -d / -f 5 | cut -d . -f 1 | xargs -L1 -i curl https://api.github.com/repos/$NAME/{} 2> /dev/null | grep size | tr -dc '[:digit:]\n' | paste -sd+ - | bc
+fi
